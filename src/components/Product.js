6438e9462777d5eb7a8e8e70483648fbedb1b993
lloyd ropa <lloyd.ropa18@gmail.com>
2025-02-27 09:38:05 +0800

    Adding data for products using props

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,23 +1,16 @@
-const Product = () => {
+const Product = ({ title, price, description, imgSrc }) => {
   return (
     <div className="product" id="product">
       <div className="product_image">
-        <img src="" alt="" />
-        Image here
+        <img src={imgSrc} alt="" />
       </div>
       <div className="product_details">
         <div className="product_details_top">
-          <h3 className="product_title">title</h3>
-          <h3 className="product_price">price</h3>
+          <h3 className="product_title">{title}</h3>
+          <h3 className="product_price">{price}</h3>
         </div>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia
-          consequatur animi vel temporibus iure accusamus aliquid nulla, fugiat,
-          labore magni officia, recusandae dolores! Exercitationem, aliquam
-          itaque delectus doloremque vero deleniti corrupti officia porro natus
-          omnis? A ea voluptas excepturi suscipit.
-        </p>
-        <p>Order a delivery</p>
+        <p>{description}</p>
+        <p>Order a delivery 🚲</p>
       </div>
     </div>
   );
--- src/components/Specials.js
+++ src/components/Specials.js
@@ -1,4 +1,7 @@
 import Product from "./Product";
+import lemonDessert from "../images/lemon_dessert.jpg";
+import bruchetta from "../images/bruchetta.svg";
+import greekSalad from "../images/greek_salad.jpg";
 const Specials = () => {
   return (
     <section className="specials_section">
@@ -11,9 +14,24 @@ const Specials = () => {
 
       <div className="specials_bottom">
         {/* Product */}
-        <Product />
-        <Product />
-        <Product />
+        <Product
+          title="Greek Salad"
+          price="$12.99"
+          imgSrc={greekSalad}
+          description="The famous greek salad of crispy lettuce, peppers, olives, and our Chicago style feta cheese, garmished with crunchy garlic and rosemary croutons"
+        />
+        <Product
+          title="Brunchetta"
+          price="$5.99"
+          imgSrc={bruchetta}
+          description="Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil."
+        />
+        <Product
+          title="Lemon Dessert"
+          price="$5.00"
+          imgSrc={lemonDessert}
+          description="This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined."
+        />
       </div>
     </section>
   );
